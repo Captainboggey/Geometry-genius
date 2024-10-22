b3634e3786546a4ba4782ef64bdb3fb6eedaa4d8
captainboogey <anik35-700@diu.edu.bd>
2024-10-23 04:50:00 +0600

    programming hero project

--- scripts/rectringlearea.js
+++ scripts/rectringlearea.js
@@ -1,3 +1,20 @@
 function calculateRectangleArea(){
-    console.log('rect');
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(typeof length)
+
+    // get width of the rectangle
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(typeof width)
+
+    // calculate rectangle area
+    const area = length*width;
+
+    const rectangleAreaSpan =document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
+
+
 }
